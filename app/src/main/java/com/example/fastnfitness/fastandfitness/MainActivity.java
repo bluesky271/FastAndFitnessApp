2aca2425c5a59a79d360c825a1a484101530569c
bluesky271 <ngoulong2001@yahoo.com.hk>
2018-05-17 22:25:03 +0100

    Initial commit with redirect to random direction page

---
+++ app/src/main/java/com/example/fastnfitness/fastandfitness/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.fastnfitness.fastandfitness;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
+    }
+
+    private void goToSecondActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/fastnfitness/fastandfitness/activity_three.java
@@ -0,0 +1,13 @@
+package com.example.fastnfitness.fastandfitness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_three extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three);
+    }
+}
