cd48f15eba3967f352652924942c82ad49f03db7
ngoulongkam <ngoulongkam2015@gmail.com>
2018-05-19 03:00:50 +0100

    Added imageView and assign random image to imageView on click

--- app/src/main/java/com/example/fastnfitness/fastandfitness/RandomDirectionPageActivity.java
+++ app/src/main/java/com/example/fastnfitness/fastandfitness/RandomDirectionPageActivity.java
@@ -1,13 +1,35 @@
 package com.example.fastnfitness.fastandfitness;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class RandomDirectionPageActivity extends AppCompatActivity {
+
+    final Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.random_direction_page);
+
+        final ImageView img = findViewById(R.id.imageViewExampleId);
+        final String imageFileName = "random_arrow_" + random.nextInt(4);
+        img.setImageDrawable(
+                getResources().getDrawable(getResourceID(imageFileName, getApplicationContext()))
+        );
+    }
+
+    protected static int getResourceID(final String resName, final Context ctx) {
+        final int ResourceID = ctx.getResources().getIdentifier(resName, "drawable", ctx.getApplicationInfo().packageName);
+        if (ResourceID == 0) {
+            throw new IllegalArgumentException("No resource string found with name: " + resName);
+        }
+        else {
+            return ResourceID;
+        }
     }
 }
